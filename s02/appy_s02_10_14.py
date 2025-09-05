c53d6bd40a95bae6884921d5779f1f4545f7f709
Daisuke <mhdf23888@gmail.com>
2025-09-05 13:41:25 +0800

    adding "s02/appy_s02_10_14.py" on  5/Sep/2025

---
+++ s02/appy_s02_10_14.py
@@ -0,0 +1,15 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/02/26 00:27:39 (CST) daisuke>
+#
+
+# importing math module
+import math
+
+# conversion from degree into radian
+e_deg = 180.0
+e_rad = math.radians (e_deg)
+
+# printing result
+print (f'{e_deg} deg = {e_rad} rad')
