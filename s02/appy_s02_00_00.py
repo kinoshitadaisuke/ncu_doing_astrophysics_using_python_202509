e5d9aa0b84ed770a2a8c368d33cb55a888e59e03
Daisuke <mhdf23888@gmail.com>
2025-09-05 13:39:31 +0800

    adding "s02/appy_s02_00_00.py" on  5/Sep/2025

---
+++ s02/appy_s02_00_00.py
@@ -0,0 +1,14 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/02/23 20:24:08 (CST) daisuke>
+#
+
+# importing os module
+import os
+
+# knowing where I am now
+cwd = os.getcwd ()
+
+# printing where you are now
+print (f'currently working directory = "{cwd}"')
