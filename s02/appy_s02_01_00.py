6f2fe0eb230a808e80da8cefb96fb1d98eb59267
Daisuke <mhdf23888@gmail.com>
2025-09-05 13:39:51 +0800

    adding "s02/appy_s02_01_00.py" on  5/Sep/2025

---
+++ s02/appy_s02_01_00.py
@@ -0,0 +1,14 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/02/24 21:18:16 (CST) daisuke>
+#
+
+# importing sys module
+import sys
+
+# getting Python version
+version = sys.version
+
+# printing Python version
+print (f'Python version = "{version}"')
