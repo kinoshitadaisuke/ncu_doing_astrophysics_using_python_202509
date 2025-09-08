a627a7fc52e473cb31f7d284f977b2acbb701bf0
Daisuke <mhdf23888@gmail.com>
2025-09-08 13:39:47 +0800

    adding "s02/appy_s02_13_00.py" on  8/Sep/2025

--- s02/appy_s02_13_00.py
+++ s02/appy_s02_13_00.py
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/python3.13
 
 #
-# Time-stamp: <2025/02/24 21:35:02 (CST) daisuke>
+# Time-stamp: <2025/09/08 13:11:31 (UT+08:00) daisuke>
 #
 
 # two floating point numbers "a" and "b"
