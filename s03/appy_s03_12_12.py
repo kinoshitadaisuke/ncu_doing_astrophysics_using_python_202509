e11d4bf206c65f7c47fddbf75ee831d243f79cc4
Daisuke <mhdf23888@gmail.com>
2025-09-11 21:22:39 +0800

    adding "s03/appy_s03_12_12.py" on 11/Sep/2025

---
+++ s03/appy_s03_12_12.py
@@ -0,0 +1,20 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/03/04 21:42:19 (CST) daisuke>
+#
+
+# importing Numpy module
+import numpy
+
+# input data file
+file_input = 'numpy_04.npz'
+
+# loading Numpy arrays from npy file
+arrays = numpy.load (file_input)
+
+# printing "arrays"
+print (f'{arrays}')
+
+# printing object type of "arrays"
+print (f'{type (arrays)}')
