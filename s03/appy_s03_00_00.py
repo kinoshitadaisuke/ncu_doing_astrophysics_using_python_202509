2433173c81fb655e7ee67909cf8748b19ca1b4c4
Daisuke <mhdf23888@gmail.com>
2025-09-11 21:19:45 +0800

    adding "s03/appy_s03_00_00.py" on 11/Sep/2025

---
+++ s03/appy_s03_00_00.py
@@ -0,0 +1,14 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/03/02 11:14:34 (CST) daisuke>
+#
+
+# importing Numpy module
+import numpy
+
+# making a Numpy array (ndarray)
+array_a = numpy.array ([0.1, 2.3, 4.5, 6.7, 8.9])
+
+# printing Numpy array
+print (f'array_a = {array_a}')
