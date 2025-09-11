23e333020c1884c5431eec019396aabdc2bf5c71
Daisuke <mhdf23888@gmail.com>
2025-09-11 21:20:50 +0800

    adding "s03/appy_s03_04_05.py" on 11/Sep/2025

---
+++ s03/appy_s03_04_05.py
@@ -0,0 +1,22 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/03/02 12:59:17 (CST) daisuke>
+#
+
+# importing numpy module
+import numpy
+
+# making a Numpy array
+a = numpy.array ([ [1.0, 2.0, 3.0], [100.0, 200.0, 300.0] ])
+
+# printing Numpy array "a"
+print (f'a:')
+print (f'{a}')
+
+# making a transposed array
+b = a.T
+
+# printing Numpy array "b"
+print (f'b:')
+print (f'{b}')
