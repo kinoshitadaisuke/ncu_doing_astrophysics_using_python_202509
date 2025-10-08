1a8c202c17669cb2bdc37c1c0a5c406cd5628c8d
Daisuke <mhdf23888@gmail.com>
2025-10-08 20:55:25 +0800

    adding "appy_s07_00_00.py" on  8/Oct/2025

---
+++ s07/appy_s07_00_00.py
@@ -0,0 +1,14 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/03/26 09:21:23 (UT+08:00) daisuke>
+#
+
+# importing astropy module
+import astropy.constants
+
+# speeed of light in vacuum
+c = astropy.constants.c
+
+# printing c
+print (c)
