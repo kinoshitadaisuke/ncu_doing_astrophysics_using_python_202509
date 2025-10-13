1306288cdf3c5fc6b369b4c380339c42868d2a49
Daisuke <mhdf23888@gmail.com>
2025-10-13 14:31:37 +0800

    adding "appy_s07_00_07.py" on 13/Oct/2025

--- s07/appy_s07_00_07.py
+++ s07/appy_s07_00_07.py
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/python3.13
 
 #
-# Time-stamp: <2025/03/26 09:21:58 (UT+08:00) daisuke>
+# Time-stamp: <2025/10/13 13:13:27 (UT+08:00) daisuke>
 #
 
 # importing astropy module
@@ -17,12 +17,9 @@ R_J = astropy.constants.R_jup
 R_E = astropy.constants.R_earth
 
 # printing Solar radius, Jupiter radius, and Earth radius
-print (R_S)
-print ()
-print (R_J)
-print ()
-print (R_E)
-print ()
+print (R_S, "\n")
+print (R_J, "\n")
+print (R_E, "\n")
 
 # value of 1 Earth radius in the unit of Solar radius
 print (f'1 R_E = {R_E:g}')
