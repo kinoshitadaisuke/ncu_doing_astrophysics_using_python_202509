0bf8d889ab5321ba9ad5d40e8db795ee79ef2cd6
Daisuke <mhdf23888@gmail.com>
2025-10-13 14:32:27 +0800

    adding "appy_s07_03_01.py" on 13/Oct/2025

--- s07/appy_s07_03_01.py
+++ s07/appy_s07_03_01.py
@@ -1,14 +1,14 @@
 #!/usr/pkg/bin/python3.13
 
 #
-# Time-stamp: <2025/04/01 14:05:37 (UT+08:00) daisuke>
+# Time-stamp: <2025/10/13 13:31:47 (UT+08:00) daisuke>
 #
 
 # importing astropy module
 import astropy.time
 
 # date/time in UT as a string
-time_str = '2025-04-15T12:00:00'
+time_str = '2026-01-01T12:00:00'
 
 # constructing Astropy's Time object from a string
 time = astropy.time.Time (time_str, format='isot', scale='utc')
