12e7efc81f9b34a2bf9f463b736ec95da2aa1e33
Daisuke <mhdf23888@gmail.com>
2025-10-30 18:41:31 +0800

    adding "s10/appy_s10_01_03.py" on 30/Oct/2025

---
+++ s10/appy_s10_01_03.py
@@ -0,0 +1,17 @@
+#!/usr/pkg/bin/python3.13
+
+#
+# Time-stamp: <2025/05/04 11:19:38 (UT+08:00) daisuke>
+#
+
+# importing astropy module
+import astropy.io.ascii
+
+# CSV file name
+file_csv = 'honey-badger/examples/planets/planets.csv'
+
+# reading a CSV file and storing data in an astropy table
+table = astropy.io.ascii.read (file_csv, format='csv')
+
+# printing the column for mean temperature
+print (f'{table["Planet", "Mean Temperature (C)"]}')
