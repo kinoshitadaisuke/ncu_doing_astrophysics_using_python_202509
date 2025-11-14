333b95a7c990e4782c330b5a0e68a0fe36090942
Daisuke <mhdf23888@gmail.com>
2025-11-14 15:58:18 +0800

    adding "appy_s12_01_00.py" on 14/Nov/2025

--- s12/appy_s12_01_00.py
+++ s12/appy_s12_01_00.py
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/python3.13
 
 #
-# Time-stamp: <2025/11/14 13:07:43 (UT+08:00) daisuke>
+# Time-stamp: <2025/11/14 15:45:03 (UT+08:00) daisuke>
 #
 
 # importing numpy module
@@ -50,12 +50,12 @@ start_calib = (24 - 8) / 24.0 # 16:00 UT
 end_calib   = (25 - 8) / 24.0 # 17:00 UT
 
 # start of observation
-date_start = '2025-01-01T12:00:00'
+date_start = '2026-01-01T12:00:00'
 t_start    = astropy.time.Time (date_start, scale='utc', format='isot')
 mjd_start  = t_start.mjd
 
 # end of observation
-date_end = '2025-01-04T20:00:00'
+date_end = '2026-01-04T20:00:00'
 t_end    = astropy.time.Time (date_end, scale='utc', format='isot')
 mjd_end  = t_end.mjd
 
