841d16390167a92dc0c88f3685544fdb500a7b01
Daisuke <mhdf23888@gmail.com>
2025-11-20 22:08:55 +0800

    adding "appy_s13_01_01.py" on 20/Nov/2025

--- s13/appy_s13_01_01.py
+++ s13/appy_s13_01_01.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.13
+#!/usr/pkg/bin/python3
 
 #
-# Time-stamp: <2025/11/19 09:56:06 (UT+08:00) daisuke>
+# Time-stamp: <2025/11/20 21:58:13 (UT+08:00) daisuke>
 #
 
 # importing argparse module
