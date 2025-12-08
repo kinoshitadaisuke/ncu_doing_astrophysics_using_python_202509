cd065d4812824b23181387c61350a8efb6a179c8
Daisuke <mhdf23888@gmail.com>
2025-12-08 23:00:01 +0800

    adding "appy_s14_00_00.py" on  8/Dec/2025

--- s14/appy_s14_00_00.py
+++ s14/appy_s14_00_00.py
@@ -1,7 +1,7 @@
-#!/usr/pkg/bin/python3.13
+#!/usr/pkg/bin/python3
 
 #
-# Time-stamp: <2025/05/27 20:39:13 (UT+08:00) daisuke>
+# Time-stamp: <2025/12/08 22:53:54 (UT+08:00) daisuke>
 #
 
 # importing argparse module
